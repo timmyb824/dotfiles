30405aab1a5d1cf8be58b0f785319b35a741b7c1
Tim Bryant <timothybryant3@gmail.com>
2023-11-24 14:46:29 -0500

    refactor: add installation of basher if not installed

--- dot_config/bin/basher.sh
+++ dot_config/bin/basher.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# Check if basher is installed
-if ! command -v basher &> /dev/null
-then
-    echo "basher could not be found"
-    exit
+# Check if git is installed
+if ! command -v git &>/dev/null; then
+    echo "git is not installed. Please install git first."
+    exit 1
+fi
+
+# Check if basher is not already installed
+if [ ! -d "$HOME/.basher" ]; then
+    echo "basher is not installed. Installing now..."
+    if git clone --depth=1 https://github.com/basherpm/basher.git ~/.basher; then
+        echo "basher installed successfully"
+    else
+        echo "basher could not be installed"
+        exit 1
+    fi
+else
+    echo "basher is already installed at $HOME/.basher"
 fi
 
 # List of packages to install
@@ -26,9 +38,16 @@ packages=(
     "vaniacer/sshto"
 )
 
+basher_bin="$HOME/.basher/bin"
+
+# Check if basher's bin directory is in the PATH
+if [[ ":$PATH:" != *":$basher_bin:"* ]]; then
+    # Add basher to PATH
+    export PATH="$basher_bin:$PATH"
+fi
+
 # Iterate over the packages and install one by one
-for package in "${packages[@]}"
-do
+for package in "${packages[@]}"; do
     if basher install "${package}"; then
         echo "${package} installed successfully"
     else
