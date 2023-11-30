28d1d7c2c54bcb22f57944d24bba78f3055010f1
Tim Bryant <timothybryant3@gmail.com>
2023-11-30 14:01:35 -0500

    style: Improve readability of list_local_bin function

--- dot_config/zsh/executable_functions.zsh
+++ dot_config/zsh/executable_functions.zsh
@@ -1,4 +1,11 @@
 list_local_bin() {
     echo "Listing all non-symlinked executables in ~/.local/bin"
-    find ~/.local/bin -type f -executable -printf "%f\n" | sort
+
+    # Define the list of executables to ignore
+    ignore_list=""
+
+    # Prepare the grep command to ignore the executables
+    ignore_grep=$(echo "$ignore_list" | tr ' ' '|')
+
+    find ~/.local/bin -type f -executable -printf "%f\n" | grep -vE "$ignore_grep" | sort
 }
