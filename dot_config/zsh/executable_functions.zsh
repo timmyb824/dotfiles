5a34548cd8ffa89a615d3a1581f1c73c5c5fa2fe
Tim Bryant <timothybryant3@gmail.com>
2023-11-30 14:22:41 -0500

    chore(alias.zsh): add 'reexec' alias to execute zsh again to reload configuration without restarting the shell chore(alias.zsh): change 'zshconfig' alias to open .zshrc file in 'micro' text editor instead of 'nano' feat(executable_functions.zsh): add 'extract' function to extract various file types based on their extension feat(executable_functions.zsh): add 'mkcd' function to create a new directory and change into it feat(executable_functions.zsh): add 'git_branch' function to print the current git branch in a clean format chore(plugin_manager.zsh): remove trailing newline at the end of the file

--- dot_config/zsh/executable_functions.zsh
+++ dot_config/zsh/executable_functions.zsh
@@ -1,3 +1,4 @@
+# list non-symlinked and non-ignored executables in .local/bin
 list_local_bin() {
     echo "Listing all non-symlinked executables in ~/.local/bin"
 
@@ -8,4 +9,42 @@ list_local_bin() {
     ignore_grep=$(echo "$ignore_list" | tr ' ' '|')
 
     find ~/.local/bin -type f -executable -printf "%f\n" | grep -vE "$ignore_grep" | sort
+}
+
+
+# extract many different file types
+# usage: `extract filename`
+extract() {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)   tar xjf $1     ;;
+            *.tar.gz)    tar xzf $1     ;;
+            *.tar.xz)    tar xJf $1     ;;
+            *.bz2)       bunzip2 $1     ;;
+            *.rar)       unrar e $1     ;;
+            *.gz)        gunzip $1      ;;
+            *.tar)       tar xf $1      ;;
+            *.tbz2)      tar xjf $1     ;;
+            *.tgz)       tar xzf $1     ;;
+            *.zip)       unzip $1       ;;
+            *.Z)         uncompress $1  ;;
+            *.7z)        7z x $1        ;;
+            *)           echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
+
+# create new directory and cd into it
+mkcd() {
+    mkdir -p -- "$1" &&
+    cd -P -- "$1"
+}
+
+
+# print current git branch in a clean way
+git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
