8e9d3fd5c52897d3d000fd7a6cfd36e8b2eb7fec
Tim Bryant <timothybryant3@gmail.com>
2024-01-20 02:49:09 -0500

    refactor: Refactor rbenv installation functions for macOS and Linux

--- dot_config/bin/installers/rbenv_ruby.sh
+++ dot_config/bin/installers/rbenv_ruby.sh
@@ -3,8 +3,14 @@
 # Define safe_remove_command function and other necessary utilities
 source "$(dirname "$BASH_SOURCE")/../utilities/init.sh"
 
-# Function to install rbenv using the official installer script
-install_rbenv() {
+# Function to install rbenv using Homebrew on macOS
+install_rbenv_macos() {
+  echo_with_color "32" "Installing rbenv using Homebrew..."
+  brew install rbenv ruby-build || exit_with_error "Failed to install rbenv and/or ruby-build."
+}
+
+# Function to install rbenv using the official installer script on Linux
+install_rbenv_linux() {
   # Install dependencies for rbenv and Ruby build
   sudo apt update || exit_with_error "Failed to update apt."
   sudo apt install -y git curl autoconf bison build-essential libssl-dev libyaml-dev \
@@ -15,14 +21,18 @@ install_rbenv() {
 
 # Function to initialize rbenv within the script
 initialize_rbenv() {
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
+  if [[ "$(get_os)" == "MacOS" ]]; then
+    eval "$(rbenv init -)"
+  elif [[ "$(get_os)" == "Linux" ]]; then
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    eval "$(rbenv init -)"
+  fi
 }
 
 # Function to install Ruby and set it as the global version
 install_and_set_ruby() {
-  rbenv install $RUBY_VERSION
-  rbenv global $RUBY_VERSION
+  rbenv install $RUBY_VERSION || exit_with_error "Failed to install Ruby version $RUBY_VERSION."
+  rbenv global $RUBY_VERSION || exit_with_error "Failed to set Ruby version $RUBY_VERSION as global."
   echo "Ruby installation completed. Ruby version set to $RUBY_VERSION."
 }
 
@@ -30,16 +40,14 @@ install_and_set_ruby() {
 if [[ "$(get_os)" == "MacOS" ]]; then
   # macOS system
   if command_exists rbenv; then
-    echo "rbenv is already installed."
+    echo_with_color "32" "rbenv is already installed."
   else
-    echo "Please install rbenv using Homebrew:"
-    echo "  brew install rbenv"
-    exit 0
+    install_rbenv_macos
   fi
 elif [[ "$(get_os)" == "Linux" ]]; then
   # Linux system
   if ! command_exists rbenv; then
-    install_rbenv
+    install_rbenv_linux
   fi
 else
   echo "Unsupported OS type: $OSTYPE"
@@ -47,7 +55,7 @@ else
 fi
 
 # Initialize rbenv for this script session
-initialize_rbenv || exit_with_error "Failed to initialize rbenv."
+initialize_rbenv
 
 # Install Ruby and set the global version
-install_and_set_ruby || exit_with_error "Failed to install Ruby."
+install_and_set_ruby
