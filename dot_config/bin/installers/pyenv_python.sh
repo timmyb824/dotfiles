f6415b0a0741b7d36ffb872c169fbdfaf4fa43b1
Tim Bryant <timothybryant3@gmail.com>
2024-01-21 02:22:41 -0500

    style: Remove unnecessary code in pyenv_python.sh

--- dot_config/bin/installers/pyenv_python.sh
+++ dot_config/bin/installers/pyenv_python.sh
@@ -30,8 +30,6 @@ install_pyenv_macos() {
         # Attempt to initialize Homebrew if it's installed but not in the PATH
         if [[ -x "/opt/homebrew/bin/brew" ]]; then
             eval "$(/opt/homebrew/bin/brew shellenv)"
-        elif [[ -x "/usr/local/bin/brew" ]]; then
-            eval "$(/usr/local/bin/brew shellenv)"
         else
             # Homebrew is not installed, provide instructions to install it
             echo_with_color "33" "Homebrew is not installed. Please run homebrew.sh first."
