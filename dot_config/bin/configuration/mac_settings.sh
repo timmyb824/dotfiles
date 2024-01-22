672acbb2d8c4633a88fd1bf5c676855261cd6a76
Tim Bryant <timothybryant3@gmail.com>
2024-01-22 11:44:16 -0500

    feat(mac_settings.sh): add script to turn off natural scrolling on macOS feat(Brewfile): add iterm2 as a cask package feat(Default_profile_iterm2_berkeley_font_20240122.json): add default profile with berkeley font settings for iterm2 feat(com.googlecode.iterm2.plist): add linux-specific commands to the list of custom commands for iterm2

---
+++ dot_config/bin/configuration/mac_settings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$BASH_SOURCE")/../utilities/init.sh"
+
+# Turn off natural scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
