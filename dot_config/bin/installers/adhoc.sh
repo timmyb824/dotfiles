4383bed1c50637e9fdbbd15f775821680a52fb5a
Tim Bryant <timothybryant3@gmail.com>
2024-02-01 02:13:03 -0500

    feat(installers): add adhoc.sh script for installing Termium- Adds a new installer script for Termium- Determines the download tool (curl or wget) to download the installation script- Prints a message if Termium is already installed

---
+++ dot_config/bin/installers/adhoc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$BASH_SOURCE")/../utilities/init.sh"
+
+install_termium() {
+    if command_exists termium; then
+        echo_with_color "33" "Termium is already installed."
+        exit 0
+    else
+        echo_with_color "32" "Installing Termium..."
+        # Determine download tool
+        if command_exists curl; then
+            curl -L https://github.com/Exafunction/codeium/releases/download/termium-v0.2.0/install.sh | bash
+        elif command_exists wget; then
+            wget -O - https://github.com/Exafunction/codeium/releases/download/termium-v0.2.0/install.sh | bash
+        else
+            exit_with_error "Error: 'curl' or 'wget' is required to download files."
+        fi
+    fi
+}
