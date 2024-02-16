80108cc66a21742229cb4386b6164f567e6ff2d1
Tim Bryant <timothybryant3@gmail.com>
2024-02-16 18:46:57 -0500

    fix: Update atuin login username to "tbryant"

--- dot_config/bin/installers/atuin.sh
+++ dot_config/bin/installers/atuin.sh
@@ -11,13 +11,11 @@ if command_exists atuin; then
     echo_with_color "32" "atuin is already installed"
     if atuin status | grep -q "session not found"; then
         echo_with_color "33" "atuin is not logged in"
-        atuin login -u "$ATUIN_USERNAME"
+        atuin login -u "tbryant"
     else
         echo_with_color "32" "atuin is already logged in"
     fi
 else
-    echo_with_color "31" "atuin could not be found"
-
     # Attempt to fix atuin command availability
     attempt_fix_command atuin "$HOME/.local/bin"
 
@@ -25,7 +23,7 @@ else
     if command_exists atuin; then
         echo_with_color "33" "Found atuin, initializing..."
         initialize_atuin
-        atuin login -u "$ATUIN_USERNAME"
+        atuin login -u "tbryant"
     else
         exit_with_error "atuin is still not found after attempting to fix the PATH. Please install atuin to continue."
     fi
