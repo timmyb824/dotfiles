2a97e7d20eb543989964ec44143e6bc75caaf33a
Tim Bryant <timothybryant3@gmail.com>
2024-02-17 12:13:25 -0500

    fix: Correct script path for installing Node.js using fnm

--- dot_config/bin/install.sh
+++ dot_config/bin/install.sh
@@ -32,7 +32,7 @@ run_script package-managers/micro.sh
 run_script package-managers/pipx.sh
 run_script installers/pyenv_python.sh
 run_script package-managers/pip.sh
-run_script package-managers/fnm_node.sh
+run_script installers/fnm_node.sh
 run_script package-managers/npm.sh
 run_script installers/tfenv_terraform.sh
 run_script installers/tailscale.sh
