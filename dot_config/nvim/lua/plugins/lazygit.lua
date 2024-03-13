380db520f3304cf4f20bea33e0b4964920911409
Tim Bryant <timothy.bryant@simplisafe.com>
2024-03-13 09:42:56 -0400

    feat: add new plugins

---
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+        "nvim-lua/plenary.nvim",
+        },
+    config = function()
+    require("lazy").setup()
+end,
+}
