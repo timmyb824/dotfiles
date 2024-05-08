07ad7a535a4bd0ac9e9f6b861687c1c4b8b202a3
Tim Bryant <timothybryant3@gmail.com>
2024-05-08 12:47:25 -0400

    feat: Add configurations for new plugins and language server

---
+++ dot_config/nvim/lua/tbryant/plugins/barbecue.lua
@@ -0,0 +1,12 @@
+return {
+	"utilyre/barbecue.nvim",
+	Lazy = true,
+	name = "barbecue",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		-- configurations go here
+	},
+}
