b1b964acf3ab585eca47640013099f1e4e56c429
Tim Bryant <timothybryant3@gmail.com>
2024-05-10 13:09:29 -0400

    refactor: Update key bindings and add more descriptive names for which-key commands

--- dot_config/nvim/lua/tbryant/plugins/which-key.lua
+++ dot_config/nvim/lua/tbryant/plugins/which-key.lua
@@ -1,13 +1,44 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 500
+    end,
+    opts = {
+        defaults = {
+            ["<leader>c"] = {
+                name = "+code"
+            },
+            ["<leader>e"] = {
+                name = "+file explorer"
+            },
+            ["<leader>f"] = {
+                name = "+fuzzy find"
+            },
+            ["<leader>m"] = {
+                name = "+format visual mode"
+            },
+            ["<leader>n"] = {
+                name = "+clear search"
+            },
+            ["<leader>s"] = {
+                name = "+split windows"
+            },
+            ["<leader>t"] = {
+                name = "+tab management"
+            },
+            ["<leader>w"] = {
+                name = "+session"
+            },
+            ["<leader>x"] = {
+                name = "+troubleshoot"
+            }
+        }
+    },
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+        wk.register(opts.defaults)
+    end
 }
