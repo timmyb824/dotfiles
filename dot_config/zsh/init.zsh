f32f8eed77655abe712856a4881ad290bea79e4c
Tim Bryant <timothybryant3@gmail.com>
2024-05-13 15:39:14 -0400

    style: Update iterm configuration and zsh settings without changing code functionality

--- dot_config/zsh/init.zsh
+++ dot_config/zsh/init.zsh
@@ -14,4 +14,5 @@ if [[ $TERM_PROGRAM != "WarpTerminal" ]]; then
     export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
     eval "$(atuin init zsh)"
     test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=white,bold,bg=c0c0c0,bold"
 fi
