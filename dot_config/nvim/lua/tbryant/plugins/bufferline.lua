83c67821bc1d689cd52fa14b01cbf5ee9c41fede
Tim Bryant <timothybryant3@gmail.com>
2024-05-18 13:19:09 -0400

    chore(nvim): update coc-settings.json to use template for hostname-based configuration selection chore(nvim): refactor bufferline.lua to fix compatibility issue with nvim 0.10.0

--- dot_config/nvim/lua/tbryant/plugins/bufferline.lua
+++ dot_config/nvim/lua/tbryant/plugins/bufferline.lua
@@ -1,11 +1,25 @@
+-- Seems to have broke with nvim 0.10.0
+-- return {
+--   "akinsho/bufferline.nvim",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   version = "*",
+--   opts = {
+--     options = {
+--       mode = "tabs",
+--       separator_style = "slant",
+--     },
+--   },
+-- }
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+    require("lazy").setup({
+      {
+      "willothy/nvim-cokeline",
+      dependencies = {
+        "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
+        "nvim-tree/nvim-web-devicons", -- If you want devicons
+      --  "stevearc/resession.nvim"       -- Optional, for persistent history
+      },
+      config = true
+    }
+    })
 }
