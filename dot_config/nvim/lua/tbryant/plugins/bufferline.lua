5c1d58bc5097bfa68577cd2e510d665941d8fb0b
Tim Bryant <timothybryant3@gmail.com>
2024-05-27 23:17:51 -0400

    refactor: Remove commented-out code and organize bufferline setup configuration

--- dot_config/nvim/lua/tbryant/plugins/bufferline.lua
+++ dot_config/nvim/lua/tbryant/plugins/bufferline.lua
@@ -1,25 +1,25 @@
 -- Seems to have broke with nvim 0.10.0
--- return {
---   "akinsho/bufferline.nvim",
---   dependencies = { "nvim-tree/nvim-web-devicons" },
---   version = "*",
---   opts = {
---     options = {
---       mode = "tabs",
---       separator_style = "slant",
---     },
---   },
--- }
 return {
-    require("lazy").setup({
-      {
-      "willothy/nvim-cokeline",
-      dependencies = {
-        "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
-        "nvim-tree/nvim-web-devicons", -- If you want devicons
-      --  "stevearc/resession.nvim"       -- Optional, for persistent history
-      },
-      config = true
-    }
-    })
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = "slant",
+    },
+  },
 }
+-- return {
+--     require("lazy").setup({
+--       {
+--       "willothy/nvim-cokeline",
+--       dependencies = {
+--         "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
+--         "nvim-tree/nvim-web-devicons", -- If you want devicons
+--       --  "stevearc/resession.nvim"       -- Optional, for persistent history
+--       },
+--       config = true
+--     }
+--     })
+-- }
