8f8bb3e44e2c7cfc437ee29e07e01b28ec89b2a5
Tim Bryant <timothybryant3@gmail.com>
2024-07-15 12:33:03 -0500

    fix conflicts

--- dot_config/zsh/completions.zsh
+++ dot_config/zsh/completions.zsh
@@ -1,3 +1,7 @@
+{{- if eq .chezmoi.os "linux" -}}
+[[ -e "/home/tbryant/oracle-cli/lib/python3.11/site-packages/oci_cli/bin/oci_autocomplete.sh" ]] && source "/home/tbryant/oracle-cli/lib/python3.11/site-packages/oci_cli/bin/oci_autocomplete.sh"
+{{- end }}
+
 eval "$(op completion zsh)"; compdef _op op
 source <(chezmoi completion zsh)
 
