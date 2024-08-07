35cc55c396106ca71e07fc55cdc92a1ff9c6e1b4
Tim Bryant <timothybryant3@gmail.com>
2024-08-07 12:31:09 -0400

    feat: Add common functions for logging to scripts

--- dot_config/bin/utilities/executable_cloudflare_dns.sh
+++ dot_config/bin/utilities/executable_cloudflare_dns.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./common.sh
+
 # confirm an argument is passed
 if [ -z "$1" ]; then
   echo "Usage: $0 <add|remove>"
@@ -8,15 +10,15 @@ fi
 
 # if arg is `add` then prepend 1.1.1.1 to top of resolv.conf
 if [ "$1" == "add" ]; then
-  echo "Adding 1.1.1.1 to resolv.conf..."
+  msg_info "Adding 1.1.1.1 to resolv.conf..."
   # Create a temporary file with the new nameserver at the top
   sudo sed -i '1inameserver 1.1.1.1' /etc/resolv.conf
-  echo "Done"
+  msg_ok "Done"
 fi
 
 # if arg is `remove` then remove 1.1.1.1 from resolv.conf
 if [ "$1" == "remove" ]; then
-  echo "Removing 1.1.1.1 from resolv.conf..."
+  msg_info "Removing 1.1.1.1 from resolv.conf..."
   sudo sed -i '/^nameserver 1.1.1.1$/d' /etc/resolv.conf
-  echo "Done"
+  msg_ok "Done"
 fi
--- dot_config/bin/utilities/executable_podman_unit_delete_service_file.sh
+++ dot_config/bin/utilities/executable_podman_unit_delete_service_file.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./common.sh
+
 # Check if a service name was provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <service-name>"
@@ -11,23 +13,30 @@ SERVICE_NAME="$1"
 UNIT_FILE="container-$SERVICE_NAME.service"
 
 # Stop the user service
-systemctl --user stop "$UNIT_FILE"
+msg_info "Stopping user service $UNIT_FILE"
+systemctl --user stop "$UNIT_FILE" || msg_warn "User service $UNIT_FILE is not running."
 
 # Disable the user service
-systemctl --user disable "$UNIT_FILE"
+msg_info "Disabling user service $UNIT_FILE"
+systemctl --user disable "$UNIT_FILE" || msg_warn "User service $UNIT_FILE is not enabled."
 
 # Remove the user service file
-rm -f "$HOME/.config/systemd/user/$UNIT_FILE"
+msg_info "Removing user service file $HOME/.config/systemd/user/$UNIT_FILE"
+rm -f "$HOME/.config/systemd/user/$UNIT_FILE" || msg_warn "User service file $HOME/.config/systemd/user/$UNIT_FILE does not exist."
 
 # Reload the daemon in case the file removal needs to be recognized
+msg_info "Reloading user daemon"
 systemctl --user daemon-reload
+
+msg_info "Resetting failed state of $UNIT_FILE"
 systemctl --user reset-failed "$UNIT_FILE"
 
 # Stop and remove the container
-podman stop "$SERVICE_NAME" || echo "Container $SERVICE_NAME is not running."
-podman rm "$SERVICE_NAME" || echo "Container $SERVICE_NAME does not exist."
+msg_info "Stopping and removing container $SERVICE_NAME"
+podman stop "$SERVICE_NAME" || msg_warn "Container $SERVICE_NAME is not running."
+podman rm "$SERVICE_NAME" || msg_warn "Container $SERVICE_NAME does not exist."
 
-echo "Service and container for $SERVICE_NAME have been removed."
+msg_ok "Service and container for $SERVICE_NAME have been removed."
 
-echo "Remaining user services:"
+msg_info "Remaining user services:"
 systemctl --user list-units 'container*'
