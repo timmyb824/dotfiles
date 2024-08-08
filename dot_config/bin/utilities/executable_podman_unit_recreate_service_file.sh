f402b4e2c14d17c18ae42262a35edb3b769a022e
Tim Bryant <timothybryant3@gmail.com>
2024-08-08 15:27:29 -0400

    refactor: Add logging messages for various functions and remove unnecessary color echo functions

--- dot_config/bin/utilities/executable_podman_unit_recreate_service_file.sh
+++ dot_config/bin/utilities/executable_podman_unit_recreate_service_file.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source common.sh
+
 # Check if both arguments are provided
 if [ $# -ne 2 ]; then
   echo "Usage: $0 [docker|podman] <service_name>"
@@ -12,6 +14,7 @@ COMPOSE_FILE="${COMPOSE_TYPE}-compose.yaml"
 
 # Function to stop and disable systemd service
 stop_and_disable_service() {
+    msg_info "Stopping and disabling the systemd service"
     local service_name="$1"
     systemctl --user stop "container-$service_name.service"
     systemctl --user disable "container-$service_name.service"
@@ -19,18 +22,21 @@ stop_and_disable_service() {
 
 # Function to remove existing container
 remove_container() {
+    msg_info "Removing the existing containers"
     local project_name="$1"
     podman-compose -p "$project_name" down
 }
 
 # Function to create and start new container with updated configuration
 recreate_container() {
+    msg_info "Recreating the containers with the new configuration"
     local project_name="$1"
     podman-compose -f "$COMPOSE_FILE" -p "$project_name" up -d --force-recreate
 }
 
 # Function to regenerate systemd unit file
 regenerate_systemd_unit_file() {
+    msg_info "Regenerating and enabling the systemd unit file"
     local service_name="$1"
 
     # Assuming the presence of a utility script to regenerate unit file here
@@ -39,30 +45,11 @@ regenerate_systemd_unit_file() {
     systemctl --user enable --now "container-$service_name.service"
 }
 
-echo_color() {
-    local color="$1"
-    local message="$2"
-    echo -e "\e[${color}m${message}\e[0m"
-}
-
-echo_color 32 "Updating service $SERVICE_NAME with $COMPOSE_TYPE"
-
-echo_color 32 "Stopping and disabling the systemd service"
-# Stop and disable the systemd service
+msg_info "Recreating user service: $SERVICE_NAME"
 stop_and_disable_service "$SERVICE_NAME"
-
-echo_color 32 "Removing the existing containers"
-# Remove the existing containers
 remove_container "$SERVICE_NAME"
-
-echo_color 32 "Recreating the containers with the new configuration"
-# Recreate the containers with the new configuration
 recreate_container "$SERVICE_NAME"
-
-echo_color 32 "Regenerating and enabling the systemd unit file"
-# Regenerate and enable the systemd unit file
 regenerate_systemd_unit_file "$SERVICE_NAME"
 
-echo_color 32 "Service $SERVICE_NAME updated successfully"
-# List the systemd units to verify
+msg_info "Listing systemd containers"
 systemctl --user list-units 'container*'
