af7e44393174284208b506d555dfae1e040780eb
Tim Bryant <timothybryant3@gmail.com>
2025-01-29 09:31:02 -0500

    feat: Add initial configuration files for AstroVim plugin setup

---
+++ dot_config/nvim-astrovim/lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
+}
