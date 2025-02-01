de3d88149667ca245bbc085637e68f507671f840
Tim Bryant <timothybryant3@gmail.com>
2025-02-01 03:17:00 -0500

    refactor: Update configuration in indent-rainbowline.lua

--- dot_config/nvim-astrovim/lua/plugins/indent-rainbowline.lua
+++ dot_config/nvim-astrovim/lua/plugins/indent-rainbowline.lua
@@ -1,7 +1,19 @@
+-- return {
+--     "lukas-reineke/indent-blankline.nvim",
+--     opts = function(_, opts)
+--         return require("indent-rainbowline").make_opts(opts, require("astrocore").plugin_opts "indent-rainbowline.nvim")
+--     end,
+--     dependencies = { "TheGLander/indent-rainbowline.nvim" },
+-- }
+
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = function(_, opts)
-        return require("indent-rainbowline").make_opts(opts, require("astrocore").plugin_opts "indent-rainbowline.nvim")
-    end,
-    dependencies = { "TheGLander/indent-rainbowline.nvim" },
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = function(_, opts)
+		-- Other blankline configuration here
+		return require("indent-rainbowline").make_opts(opts)
+	end,
+	dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
 }
