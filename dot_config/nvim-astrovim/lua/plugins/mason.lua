f3e5fbb0a28913142141ea2ab28a305e984e654d
Tim Bryant <Timothybryant3@gmail.com>
2025-03-27 12:30:24 -0400

    feat: add mason packages to astrovim

--- dot_config/nvim-astrovim/lua/plugins/mason.lua
+++ dot_config/nvim-astrovim/lua/plugins/mason.lua
@@ -1,4 +1,43 @@
----if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- ---if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- -- Customize Mason plugins
+
+-- ---@type LazySpec
+-- return {
+--   -- use mason-lspconfig to configure LSP installations
+--   {
+--     "williamboman/mason-lspconfig.nvim",
+--     -- overrides `require("mason-lspconfig").setup(...)`
+--     opts = {
+--       ensure_installed = {
+--         "lua_ls",
+--         -- add more arguments for adding more language servers
+--       },
+--     },
+--   },
+--   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+--   {
+--     "jay-babu/mason-null-ls.nvim",
+--     -- overrides `require("mason-null-ls").setup(...)`
+--     opts = {
+--       ensure_installed = {
+--         "stylua",
+--         -- add more arguments for adding more null-ls sources
+--       },
+--       automatic_installation = true,
+--     },
+--   },
+--   {
+--     "jay-babu/mason-nvim-dap.nvim",
+--     -- overrides `require("mason-nvim-dap").setup(...)`
+--     opts = {
+--       ensure_installed = {
+--         "python",
+--         -- add more arguments for adding more debuggers
+--       },
+--     },
+--   },
+-- }
 
 -- Customize Mason plugins
 
@@ -10,8 +49,27 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
+        "bashls",
+        "dockerls",
+        "gopls",
+        "helm_ls",
+        "jedi_language_server",
+        "jinja_lsp",
+        "jsonls",
         "lua_ls",
-        -- add more arguments for adding more language servers
+        "marksman",
+        "pyright",
+        "pylyzer",
+        "pylsp",
+        "ts_ls",
+        -- "ruby_lsp", -- TODO: Install error; install manually
+        "ruff",
+        -- "ruff_lsp", -- TODO: Install error; install manually
+        "rust_analyzer",
+        "sourcery",
+        "terraformls",
+        "typos_lsp",
+        "yamlls",
       },
     },
   },
@@ -21,8 +79,34 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
+        "ansible-lint",
+        "beautysh",
+        "black",
+        "codespell",
+        "cspell",
+        "dotenv-linter",
+        "eslint_d",
+        "gitlint",
+        "gitleaks",
+        "goimports",
+        "gofumpt",
+        "isort",
+        "jsonlint",
+        "kube-linter",
+        "markdownlint",
+        "misspell",
+        "prettier",
+        "pylint",
+        "rubyfmt",
+        "ruff",
+        "shellcheck",
+        "shfmt",
+        "sql-formatter",
         "stylua",
-        -- add more arguments for adding more null-ls sources
+        "trufflehog",
+        "typos",
+        "yamllint",
+        "yamlfmt",
       },
       automatic_installation = true,
     },
@@ -33,7 +117,9 @@ return {
     opts = {
       ensure_installed = {
         "python",
-        -- add more arguments for adding more debuggers
+        "delve",
+        "node2",
+        "codelldb",
       },
     },
   },
