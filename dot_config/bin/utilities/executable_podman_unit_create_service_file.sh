934e14ca2089414a8b384f892619c120c4368296
Tim Bryant <timothybryant3@gmail.com>
2025-11-19 12:10:06 -0500

    refactor: Improve indentation and formatting in podman unit shell scripts

--- dot_config/bin/utilities/executable_podman_unit_create_service_file.sh
+++ dot_config/bin/utilities/executable_podman_unit_create_service_file.sh
@@ -3,35 +3,36 @@
 source common.sh
 
 if [ -z "$1" ]; then
-  echo "Usage: $0 <container_name>"
-  exit 1
+	echo "Usage: $0 <container_name>"
+	exit 1
 fi
 
 CONTAINER_NAME="$1"
 SYSTEMD_USER_DIR="$HOME/.config/systemd/user"
 
 generate_systemd_unit() {
-  local container_name="$1"
-  local restart_policy="$2"
+	local container_name="$1"
+	local restart_policy="$2"
 
-  mkdir -p "${SYSTEMD_USER_DIR}"
-  cd "${SYSTEMD_USER_DIR}" || exit
+	mkdir -p "${SYSTEMD_USER_DIR}"
+	cd "${SYSTEMD_USER_DIR}" || exit
 
-  if [ -z "${restart_policy}" ]; then
-    podman generate systemd --new --name --files "${container_name}"
-  else
-    podman generate systemd --restart-policy="${restart_policy}" --new --name --files "${container_name}"
-  fi
+	if [ -z "${restart_policy}" ]; then
+		podman generate systemd --new --name --files "${container_name}"
+	else
+		podman generate systemd --restart-policy="${restart_policy}" --new --name --files "${container_name}"
+	fi
 }
 
 enable_systemd_service() {
-  local container_name="$1"
+	local container_name="$1"
 
-  systemctl --user enable --now "container-$container_name"
+	systemctl --user enable --now "container-$container_name"
 }
 
 list_systemd_containers() {
-  systemctl --user list-units 'container*'
+	systemctl --user --no-pager --plain --full --no-legend \
+		list-units 'container*' | awk '{print $1}'
 }
 
 msg_info "Creating systemd unit file for container: ${CONTAINER_NAME}"
--- dot_config/bin/utilities/executable_podman_unit_recreate_service_file.sh
+++ dot_config/bin/utilities/executable_podman_unit_recreate_service_file.sh
@@ -3,8 +3,8 @@
 source common.sh
 
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 [docker|podman] <service_name>"
-    exit 1
+	echo "Usage: $0 [docker|podman] <service_name>"
+	exit 1
 fi
 
 COMPOSE_TYPE=$1 # 'docker' or 'podman'
@@ -12,33 +12,33 @@ SERVICE_NAME=$2
 COMPOSE_FILE="${COMPOSE_TYPE}-compose.yaml"
 
 stop_and_disable_service() {
-    msg_info "Stopping and disabling the systemd service"
-    local service_name="$1"
-    systemctl --user stop "container-$service_name.service"
-    systemctl --user disable "container-$service_name.service"
+	msg_info "Stopping and disabling the systemd service"
+	local service_name="$1"
+	systemctl --user stop "container-$service_name.service"
+	systemctl --user disable "container-$service_name.service"
 }
 
 remove_container() {
-    msg_info "Removing the existing containers"
-    local project_name="$1"
-    podman-compose -p "$project_name" down
+	msg_info "Removing the existing containers"
+	local project_name="$1"
+	podman-compose -p "$project_name" down
 }
 
 # Function to create and start new container with updated configuration
 # in-pod=0 prevents pod from being created which seems to cause problems using podman-auto-update
 recreate_container() {
-    msg_info "Recreating the containers with the new configuration"
-    local project_name="$1"
-    podman-compose -f "$COMPOSE_FILE" --in-pod=0 -p "$project_name" up -d --force-recreate
+	msg_info "Recreating the containers with the new configuration"
+	local project_name="$1"
+	podman-compose -f "$COMPOSE_FILE" --in-pod=0 -p "$project_name" up -d --force-recreate
 }
 
 regenerate_systemd_unit_file() {
-    msg_info "Regenerating and enabling the systemd unit file"
-    local service_name="$1"
+	msg_info "Regenerating and enabling the systemd unit file"
+	local service_name="$1"
 
-    podman_unit_file.sh "$service_name"
-    systemctl --user daemon-reload
-    systemctl --user enable --now "container-$service_name.service"
+	podman_unit_create_service_file.sh "$service_name"
+	systemctl --user daemon-reload
+	systemctl --user enable --now "container-$service_name.service"
 }
 
 msg_info "Recreating user service: $SERVICE_NAME"
