9121475e1497668025277b11fe5d7934befa6b9c
Tim Bryant <timothybryant3@gmail.com>
2026-03-28 16:30:55 -0400

    fix(lsp-ai.json.tmpl): update auth_token_env_var_name to use the correct credential path for OpenAI project key fix(chatgpt.lua): change api_key_cmd to reference the correct OpenAI project key credential fix(dot_opencommit.tmpl): update model to gpt-4o-mini and change API URL and key to use the correct paths for litellm-opencommit

--- dot_config/nvim-archive/nvim-custom/lua/tbryant/plugins/chatgpt.lua
+++ dot_config/nvim-archive/nvim-custom/lua/tbryant/plugins/chatgpt.lua
@@ -1,15 +1,15 @@
 return {
-	"jackMort/ChatGPT.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("chatgpt").setup({
-			api_key_cmd = "op read op://Personal/openai-api/credential --no-newline",
-		})
-	end,
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-		"folke/trouble.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+        require("chatgpt").setup({
+            api_key_cmd = "op read op://Personal/openai-project-key/credential --no-newline",
+        })
+    end,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
 }
